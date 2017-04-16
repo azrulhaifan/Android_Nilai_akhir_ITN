0826ec0c57173bfac6d45fc3841323f47c05ea7c
azrulhaifan <ahaifan@gmail.com>
2017-04-16 15:13:30 +0700

    Deklarasi Variabel

--- app/src/main/java/id/web/datakampus/nilaiakhiritn/FragmentPerhitungan.java
+++ app/src/main/java/id/web/datakampus/nilaiakhiritn/FragmentPerhitungan.java
@@ -6,12 +6,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentPerhitungan extends Fragment {
 
+    EditText et_np1,et_np2, et_np3, et_np4, et_cp1, et_cp2, et_hasil;
+
+    Button bt_hitung;
+
+    Double np1, np2, np3, np4, cp1, cp2, npa, na;
 
     public FragmentPerhitungan() {
         // Required empty public constructor
